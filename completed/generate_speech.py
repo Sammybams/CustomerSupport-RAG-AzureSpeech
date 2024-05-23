c0a672d9ce4a73edc40528dbcdc7fb17520da69d
Sammybams <samuelbamgbola@gmail.com>
2024-05-23 01:04:13 +0100

    Added starter code for speech synthesis functionality

--- completed/generate_speech.py
+++ completed/generate_speech.py
@@ -1,3 +1,35 @@
 # Define Speech synthesis model
 def speech_sythesis():
     return
+
+# Get Configuration Settings
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+import azure.cognitiveservices.speech as speechsdk
+
+# This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+speech_config = speechsdk.SpeechConfig(subscription=os.getenv("SPEECH_KEY"), region=os.getenv("SPEECH_REGION"))
+audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+
+# The neural multilingual voice can speak different languages based on the input text.
+speech_config.speech_synthesis_voice_name='en-NG-EzinneNeural'
+
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
+
+# Get text from the console and synthesize to the default speaker.
+print("Enter some text that you want to speak >")
+text = input()
+
+speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
+
+if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+    print("Speech synthesized for text [{}]".format(text))
+elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
+    cancellation_details = speech_synthesis_result.cancellation_details
+    print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+        if cancellation_details.error_details:
+            print("Error details: {}".format(cancellation_details.error_details))
+            print("Did you set the speech resource key and region values?")
